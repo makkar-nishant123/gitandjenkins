06d531bcbc6b027957c74b3ed098f2f6e55fb024
Nishant Makkar <nishantmakkar@Nishants-MacBook-Air.local>
2017-08-02 02:25:38 +0530

    first commit

---
+++ src/test/java/testgitandjenkins/testgitandjenkins1/Testgitjenkins.java
@@ -0,0 +1,12 @@
+package testgitandjenkins.testgitandjenkins1;
+
+import org.testng.annotations.Test;
+
+public class Testgitjenkins {
+
+	  @Test
+	     public void print_something()
+	     {
+	    	 System.out.println("Hello World");
+	     }
+	}
